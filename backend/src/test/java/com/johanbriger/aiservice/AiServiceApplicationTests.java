b6b3e924d2b5b8c864901a8dc09b47b20a0a8e43
Johan Briger <johanbriger@gmail.com>
2026-05-04 12:34:51 +0200

    test: fix contextLoads failure in CI"

--- backend/src/test/java/com/johanbriger/aiservice/AiServiceApplicationTests.java
+++ backend/src/test/java/com/johanbriger/aiservice/AiServiceApplicationTests.java
@@ -2,8 +2,14 @@ package com.johanbriger.aiservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "AI_API_KEY=test-key",
+        "AI_BASE_URL=http://localhost:8080",
+        "OPENROUTER_API_KEY=test-key"
+})
 class AiServiceApplicationTests {
 
     @Test
